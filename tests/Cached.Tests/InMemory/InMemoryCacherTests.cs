d3c003ec847026ed1188a37179d49f0d174a55fb
Daniel Ryhle <daniel@ryhle.se>
2020-06-04 18:18:17 +0200

    Added tests for static factory InMemoryCacher

---
+++ tests/Cached.Tests/InMemory/InMemoryCacherTests.cs
@@ -0,0 +1,41 @@
+﻿namespace Cached.Tests.InMemory
+{
+    using System;
+    using Cached.InMemory;
+    using Microsoft.Extensions.Caching.Memory;
+    using Xunit;
+
+    public class InMemoryCacherTests
+    {
+        public class Throws
+        {
+            [Fact]
+            public void If_MemoryCache_Argument_Is_Null()
+            {
+                Assert.Throws<ArgumentNullException>(() =>
+                    InMemoryCacher.New(
+                        null, 
+                        new MemoryCacheEntryOptions()));
+            }
+        }
+
+        public class ReturnInstance
+        {
+            [Fact]
+            public void As_ICacher_Typed_Object()
+            {
+                // Arrange
+                var cache = new MemoryCache(new MemoryCacheOptions());
+
+                // Act
+                var instance = InMemoryCacher.New(cache);
+
+                // Assert
+                Assert.NotNull(instance);
+
+                // Teardown
+                cache.Dispose();
+            }
+        }
+    }
+}
