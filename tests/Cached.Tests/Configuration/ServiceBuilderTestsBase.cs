3c3e84e203d06bdba4ede35d3af34c94e164bbb6
Daniel Ryhle <daniel@ryhle.se>
2020-06-10 21:45:42 +0200

    Added use-case coverage for MemoryServiceBuilder

--- tests/Cached.Tests/Configuration/ServiceBuilderTestsBase.cs
+++ tests/Cached.Tests/Configuration/ServiceBuilderTestsBase.cs
@@ -1,6 +1,55 @@
 ﻿namespace Cached.Tests.Configuration
 {
-    public abstract class ServiceBuilderTestsBase
+    using System;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Cached.Caching;
+    using Cached.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Xunit;
+
+    public abstract class ServiceBuilderTestsBase<TProvider> where TProvider : ICacheProvider
     {
+        protected readonly ServiceBuilder<TProvider> ServiceBuilder;
+
+        protected ServiceBuilderTestsBase(ServiceBuilder<TProvider> serviceBuilder)
+        {
+            ServiceBuilder = serviceBuilder;
+        }
+
+        [Fact]
+        public void AddFunctionMethod_Throws_If_KeyFactory_Is_Null()
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+                ServiceBuilder.AddFunction<string, string>(
+                    null, 
+                    (provider, key, arg) => Task.FromResult("")));
+        }
+
+        [Fact]
+        public void AddFunctionMethod_Throws_If_FetchFactory_Is_Null()
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+                ServiceBuilder.AddFunction<string, string>(
+                    arg => "",
+                    null));
+        }
+
+        [Fact]
+        public void AddFunctionMethod_Does_Not_Clear_PreExisting_Services()
+        {
+            // Arrange
+            ServiceBuilder.AddFunction<string, string>(
+                arg => "",
+                (provider, key, arg) => Task.FromResult(""));
+            var services = new ServiceCollection();
+            services.AddSingleton<object, object>(provider => "123");
+
+            // Act
+            ServiceBuilder.GetBuild()(services);
+
+            // Assert
+            Assert.Equal("123", (string)services.BuildServiceProvider().GetService<object>());
+        }
     }
 }
--- tests/Cached.Tests/Memory/MemoryServiceBuilderTests.cs
+++ tests/Cached.Tests/Memory/MemoryServiceBuilderTests.cs
@@ -1,8 +1,36 @@
 ﻿namespace Cached.Tests.Memory
 {
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Cached.Caching;
+    using Cached.Memory;
+    using Cached.Memory.Configuration;
     using Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Xunit;
 
-    public class MemoryServiceBuilderTests : ServiceBuilderTestsBase
+    public class MemoryServiceBuilderTests : ServiceBuilderTestsBase<IMemory>
     {
+        public MemoryServiceBuilderTests() : base(new MemoryServiceBuilder())
+        {
+        }
+
+        [Fact]
+        public void AddFunctionMethod_Adds_Valid_Service_Correctly()
+        {
+            // Arrange
+            ServiceBuilder.AddFunction<string, string>(
+                arg => "",
+                (provider, key, arg) => Task.FromResult(""));
+            var services = new ServiceCollection();
+
+            // Act
+            ServiceBuilder.GetBuild()(services);
+
+            // Assert
+            Assert.Equal(8, services.Count); // Includes MemoryCache and all options instances.
+            Assert.Equal(typeof(ICache<IMemory>), services[6].ServiceType); // ICache service added before ICached
+            Assert.Equal(typeof(ICached<string, string>), services[7].ServiceType); // ICached service added last.
+        }
     }
 }
