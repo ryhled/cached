345b3059a6e823c336d7daa60279d667e141066d
Daniel Ryhle <daniel@ryhle.se>
2020-06-11 23:05:40 +0200

    Added basic coverage for MemoryCacheHandler

--- tests/Cached.Tests/Memory/MemoryCacheHandlerTests.cs
+++ tests/Cached.Tests/Memory/MemoryCacheHandlerTests.cs
@@ -1,6 +1,44 @@
 ﻿namespace Cached.Tests.Memory
 {
+    using System;
+    using Cached.Memory;
+    using Microsoft.Extensions.Caching.Memory;
+    using Moq;
+    using Xunit;
+
     public class MemoryCacheHandlerTests
     {
+        public class NewMethod
+        {
+            public class Throws
+            {
+                [Fact]
+                public void If_MemoryCache_Argument_Is_Null()
+                {
+                    Assert.Throws<ArgumentNullException>(() =>
+                        MemoryCacheHandler.New(null, new MemoryCacheEntryOptions()));
+                }
+            }
+
+            [Fact]
+            public void Creates_Instance()
+            {
+                // Arrange, Act
+                var result = MemoryCacheHandler.New(new Mock<IMemoryCache>().Object);
+
+                // Assert
+                Assert.NotNull(result);
+            }
+
+            [Fact]
+            public void Creates_Instance_WithOptions_Argument()
+            {
+                // Arrange, Act
+                var result = MemoryCacheHandler.New(new Mock<IMemoryCache>().Object, new MemoryCacheEntryOptions());
+
+                // Assert
+                Assert.NotNull(result);
+            }
+        }
     }
 }
