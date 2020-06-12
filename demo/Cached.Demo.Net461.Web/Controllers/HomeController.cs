1e49f10f25beeeed8aa543702506bc770c3b3614
Daniel Ryhle <daniel@ryhle.se>
2020-06-12 22:15:40 +0200

    Added simple Net 4.6.1 Web Demo App

---
+++ demo/Cached.Demo.Net461.Web/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Cached.Demo.Net461.Web.Controllers
+{
+    using System;
+    using System.Threading.Tasks;
+    using Caching;
+    using Memory;
+
+    public class HomeController : Controller
+    {
+        private static readonly ICache<IMemory> Cache = MemoryCacheHandler.New();
+
+        public ActionResult Index()
+        {
+            ViewBag.Value = Cache.GetOrFetchAsync(
+                "key1", 
+                provider => Task.FromResult(DateTimeOffset.UtcNow.ToString())
+            ).Result;
+
+            return View();
+        }
+    }
+}
