9c118c8b8669a4ae5e01b5731c398dde336e8ce1
Daniel Ryhle <daniel@ryhle.se>
2020-06-12 15:02:39 +0200

    Added net core console demo

---
+++ demo/Cached.Demo.NetCore.Console/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Cached.Demo.NetCore.Console
+{
+    using System;
+    using System.Threading.Tasks;
+    using Memory;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using var cache = MemoryCacheHandler.New();
+
+            while (true)
+            {
+                Console.WriteLine("Press any key, used as cache key, to get data from cache (or 'q' to quit)");
+                ConsoleKeyInfo key = Console.ReadKey();
+
+                if (key.Key == ConsoleKey.Q)
+                {
+                    break;
+                }
+
+                var result = cache.GetOrFetchAsync(
+                    key.Key.ToString(),
+                    provider => Task.FromResult(DateTimeOffset.UtcNow.ToString()))
+                    .Result;
+
+                Console.WriteLine($"Value is: {result}");
+            }
+        }
+    }
+}
